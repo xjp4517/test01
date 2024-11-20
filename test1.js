8f0c5aa2163ab7f811d8a2aff22a90bca31524c5
xjp4517 <a01091282695@gmail.com>
2024-11-20 22:30:38 +0900

    Update and rename record.js to test1.js

--- test1.js
+++ test1.js
@@ -15,15 +15,28 @@ startButton.addEventListener('click', async () => {
   }
 
   // Request access to the user's microphone
-  const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const micStream = await navigator
+      .mediaDevices
+      .getUserMedia({ audio: true });
 
   // Create the microphone stream
   audioContext = new AudioContext();
-  mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(micStream);
+  mediaStreamAudioSourceNode = audioContext
+      .createMediaStreamSource(micStream);
 
   // Create and connect AudioWorkletNode 
   // for processing the audio stream
-  await audioContext.audioWorklet.addModule("my-audio-processor.js");
-  audioWorkletNode = new AudioWorkletNode(audioContext,'my-audio-processor');
+  await audioContext
+      .audioWorklet
+      .addModule("my-audio-processor.js");
+  audioWorkletNode = new AudioWorkletNode(
+      audioContext,
+      'my-audio-processor');
   micStreamAudioSourceNode.connect(audioWorkletNode);
+});
+
+stopButton.addEventListener('click', () => {
+  // Close audio stream
+  micStreamAudioSourceNode.disconnect();
+  audioContext.close();
 });

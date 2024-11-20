1d4111d8e2b191967b63d06f4c35a5c8ccf7864f
xjp4517 <a01091282695@gmail.com>
2024-11-20 23:40:45 +0900

    Update audioprocess.js

--- audioprocess.js
+++ audioprocess.js
@@ -1,4 +1,4 @@
-class MyAudioProcessor extends AudioWorkletProcessor {
+class audioprocess extends AudioWorkletProcessor {
     process(inputs, outputs, parameters) {
         const inputData = inputs[0][0];
         return true;

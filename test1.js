520ae73b98e1d66deb6bf8768a91508ab226134c
xjp4517 <a01091282695@gmail.com>
2024-11-21 10:33:58 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -7,7 +7,7 @@ let audioWorkletNode;
 
 //시작버튼 입력시
 startBtn.addEventListener('click', async () => {
-  
+  console.log("녹음 시작");
   //마이크 권한
   const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
 

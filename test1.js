0f048e46e0b8a1467b95e16346df18a54f20d6bf
xjp4517 <a01091282695@gmail.com>
2024-11-21 11:10:52 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -11,19 +11,4 @@ startBtn.addEventListener('click', async () => {
   //마이크 권한
   const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-  //음성 입력받기
-  audioContext = new AudioContext();
-  mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(micStream);
-
-  //
-  await audioContext.audioWorklet.addModule("audioprocess.js");
-  audioWorkletNode = new AudioWorkletNode(audioContext,'audioprocess');
-  mediaStreamAudioSourceNode.connect(audioWorkletNode);
-});
-
-//중지버튼 입력시
-stopBtn.addEventListener('click', () => {
-  mediaStreamAudioSourceNode.disconnect();
-  audioContext.close();
-  const micStream = await navigator.mediaDevices.getUserMedia({ audio: false });
 });
